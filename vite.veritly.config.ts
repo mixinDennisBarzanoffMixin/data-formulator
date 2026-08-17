afa989b609ee438046da243f86bb8c4e81166d26
Denis Barzanov <denis.barzanov2002@gmail.com>
2026-08-17 15:27:17 +0000

    fix: isolate project data development entry

--- vite.veritly.config.ts
+++ vite.veritly.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
     },
   ],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  optimizeDeps: { entries: [path.resolve(__dirname, "veritly.html")] },
   build: {
     outDir: path.join(__dirname, "dist-veritly"),
     emptyOutDir: true,
